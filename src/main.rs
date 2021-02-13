5988f455cf77773924104dbef5b78ec90769ce74
blatzfab <fabian.blatz@hansgrohe.com>
2021-02-13 11:18:09 +0100

    More threading

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,15 @@ use gameoflife::GameOfLife;
 use std::{thread, time};
 
 fn main() {
-    let mut gol = GameOfLife::new(1920, 1080);
+    let mut gol = GameOfLife::new(150, 60);
     gol.seed(2000);
 
-    for _i in 0..100 {
+    for i in 0..100000 {
+        gol.show();
         gol.next_generation();
+        if i % 1000 == 0 {
+            gol.seed(1500);
+        }
+        thread::sleep(time::Duration::from_millis(50));
     }
 }
